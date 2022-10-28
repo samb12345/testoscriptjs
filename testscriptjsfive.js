cc79dcc576386433495b31543ffc737d84605774
samb12345 <[bsam@live.co.uk]>
2022-10-28 21:43:09 +0100

    six

--- testscriptjsfive.js
+++
@@ -1,6 +0,0 @@
-
-console.log('second try change four')
-
-
-
-$('div.announcement-bar').prepend('<div>This is from the github script!!!<div>').css({'background-color': 'blue', 'text-align': 'center'})
---
+++ testscriptjssix.js
@@ -0,0 +1,6 @@
+
+console.log('second try change six')
+
+var header = $('div.announcement-bar')
+
+header.prepend('<div>This is from the github script!!!<div>').css({'background-color': 'blue', 'text-align': 'center'})
