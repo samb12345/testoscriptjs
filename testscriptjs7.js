8ce54c3edbe25baa02c92c03a3b675e120ab13fd
samb12345 <[bsam@live.co.uk]>
2022-10-28 21:45:11 +0100

    7

---
+++ testscriptjs7.js
@@ -0,0 +1,6 @@
+
+console.log('second try change 7')
+
+var header = $('div.announcement-bar')
+
+header.prepend('<div>This is from the github script!!!<div>').css({'background-color': 'blue', 'text-align': 'center'})
