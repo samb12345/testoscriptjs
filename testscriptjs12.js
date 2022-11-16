39f8c89e0ccd6dfe6e6882dcc30e4bf061f214c4
samb12345 <[bsam@live.co.uk]>
2022-11-16 22:42:16 +0000

    red

--- testscriptjs12.js
+++ testscriptjs12.js
@@ -25,7 +25,7 @@ var tags2 = ptags
 
 function run(){
 
-    headerw.prepend('<div>This is from the github script!!!<div>').css({'background-color': 'blue', 'text-align': 'center'})
+    headerw.prepend('<div>This is from the github script!!!<div>').css({'background-color': 'red', 'text-align': 'center'})
 
   
 
