75b29041f5cd6520c84a3a454d80341d6ed181c8
samb12345 <[bsam@live.co.uk]>
2022-11-16 22:45:20 +0000

    hello h2

---
+++ testscriptjs13.js
@@ -0,0 +1,38 @@
+
+const script = document.createElement("script")
+
+script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"
+
+script.type= "text/javascript"
+
+script.onreadystatechange = handler
+
+script.onload = handler
+
+document.getElementsByTagName("head")[0].appendChild(script)
+
+function handler(){
+
+console.log('second try change 11')
+
+var headerq = $('div.announcement-bar')
+
+var headerw = headerq
+
+var ptags = $('h2')
+
+var tags2 = ptags
+
+function run(){
+
+    headerw.prepend('<div>This is from the github script!!!<div>').css({'background-color': 'red', 'text-align': 'center'})
+
+    tags2.prepend('hello')
+
+  
+
+
+}
+
+run()
+}
