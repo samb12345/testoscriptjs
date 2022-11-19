496f3e18d09ef9198361d6b9cad9988fdae94bda
samb12345 <[bsam@live.co.uk]>
2022-11-19 21:29:00 +0000

    22

--- testscriptjs22.js
+++ testscriptjs22.js
@@ -13,7 +13,7 @@ document.getElementsByTagName("head")[0].appendChild(script)
 
 function handler(){
 
-console.log('second try change 21')
+console.log('second try change 22')
 
 var headerq = $('div.announcement-bar')
 
