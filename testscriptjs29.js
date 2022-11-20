922f2355441a87cbe2ca92f10194d124d319ab45
samb12345 <[bsam@live.co.uk]>
2022-11-20 15:43:53 +0000

    29

--- testscriptjs29.js
+++ testscriptjs29.js
@@ -13,7 +13,7 @@ document.getElementsByTagName("head")[0].appendChild(script)
 
 function handler(){
 
-console.log('second try change 28')
+console.log('second try change 29')
 
 var headerq = $('div.announcement-bar')
 
