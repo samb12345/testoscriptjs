78a85e7be323d8de4099b75ff85a81200a2c39c2
samb12345 <[bsam@live.co.uk]>
2022-11-20 15:58:07 +0000

    31

---
+++ testscriptjs31.js
@@ -0,0 +1,49 @@
+
+const script = document.createElement("script")
+
+script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"
+
+script.type= "text/javascript"
+
+script.onreadystatechange = handler
+
+script.onload = handler
+
+document.getElementsByTagName("head")[0].appendChild(script)
+
+function handler(){
+
+console.log('second try change 31')
+
+var headerq = $('div.announcement-bar')
+
+var headerw = headerq
+
+var ptags = $('h2')
+
+var tags2 = ptags
+
+var heading3 = $('h3')
+
+var productdescription = $('div.product__description')
+
+
+array1 = productdescription.children('h3')
+
+var maptest = $.map(array1, function(item, index){
+    return `<div>${item.innerHTML}</div>`
+});
+
+console.log(maptest)
+
+function run(){
+
+
+    productdescription.before(`<div>test</div>`).css({'display': 'flex', 'background-color': 'green'})
+
+    productdescription.children().remove()
+
+};
+
+run()
+}
