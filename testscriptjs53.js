f32d87dabaca378f1eca2d2b2b792d366a56f4c6
samb12345 <[bsam@live.co.uk]>
2022-11-21 12:50:52 +0000

    53

--- testscriptjs53.js
+++ testscriptjs53.js
@@ -17,7 +17,7 @@ function handler(){
 
     $('head').append('<link rel="stylesheet" type="text/css" href="script.css">');
 
-console.log('second try change 52')
+console.log('second try change 53')
 
 var headerq = $('div.announcement-bar')
 
