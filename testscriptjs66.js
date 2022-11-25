75c2a2b28a55c5d31e016f2d26a398a4d53e6349
samb12345 <[bsam@live.co.uk]>
2022-11-25 20:02:18 +0000

    66

--- testscriptjs66.js
+++ testscriptjs66.js
@@ -15,9 +15,9 @@ document.getElementsByTagName("head")[0].appendChild(script)
 
 function handler(){
 
-    $('head').append('<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/samb12345/testoscriptjs/script3.css">');
+    $('head').append('<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/samb12345/testoscriptjs/script4.css">');
 
-console.log('second try change 65')
+console.log('second try change 66')
 
 var headerq = $('div.announcement-bar')
 
